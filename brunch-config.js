ac4f3ff99c2b6668c2655076e4d9e385e42d9e0d
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-03-24 01:55:50 -0400

    adds taiansu's configs

--- brunch-config.js
+++ brunch-config.js
@@ -1,23 +1,7 @@
 exports.config = {
-  // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css",
@@ -32,8 +16,6 @@ exports.config = {
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(web\/static\/assets)/
   },
 
@@ -53,17 +35,44 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
+      ignore: [/(web\/static\/vendor)|node_modules/]
+    },
+    sass: {
+      options: {
+        includePaths: [
+          "node_modules/bootstrap-sass/assets/stylesheets",
+          "node_modules/font-awesome/scss",
+          "node_modules/toastr"
+        ], // tell sass-brunch where to look for files to @import
+      },
+      precision: 8 // minimum precision required by bootstrap-sass
+    },
+    copycat: {
+      "fonts": [
+        "node_modules/bootstrap-sass/assets/fonts/bootstrap",
+        "node_modules/font-awesome/fonts"
+      ] // copy these files into priv/static/fonts/
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      "js/app.js": [
+        "web/static/js/app",
+        "web/static/js/theme-app"
+      ]
     }
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery'
+    },
+    styles: {
+      "bootstrap-table": ["dist/bootstrap-table.css"],
+      "jquery-ui": ["themes/base/all.css"]
+    } // included these styles into the build
   }
-};
+}
